9f5e6f9b49298bb22b391ea9100c4831366af985
André Lucas <sl.andrelucas@gmail.com>
2025-07-21 23:10:38 -0300

    feat: Finishing home page

--- app.py
+++ app.py
@@ -13,7 +13,25 @@ def get_db_connection():
         database='BLOG'
     )
 
-from views import *
+def tuple_to_dict(old_tuple):
+    if not old_tuple:
+        return dict()
+
+    new_dict = dict()
+    for i, attr in enumerate(old_tuple):
+        match i:
+            case 0:
+                new_dict['article-id'] = attr
+            case 1:
+                new_dict['article-title'] = attr
+            case 2:
+                new_dict['article-content'] = attr
+            case 3:
+                new_dict['publish-date'] = attr
+
+    return new_dict
+
+from home import *
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/home.js
+++ static/home.js
@@ -1,28 +1,85 @@
 // Função para carregamento dos artigos armazenados
-function loadArticles() {
+async function loadArticles(articles=[]) {
     const articlesList = document.getElementById('articles-list');
-
+    
     articlesList.innerHTML = '';
 
-    fetch('/articles')
-    .then(res => res.json())
-    .then(articlesRes => {
-        articlesRes.forEach(article => {
-            localDate = new Date(article['publish-date']).toLocaleString()
+    if (!articles.length) {
+        const noArticle = document.createElement('p');
+        noArticle.textContent = 'Nenhum artigo encontrado!'
+        noArticle.style.margin = '0 auto';
 
-            let newArticle = document.createElement('li');
-            newArticle.textContent = `ID: ${article['article-id']}, 
-            Título: ${article['article-title']},
-            Data de publicação: ${localDate}`;
+        articlesList.innerHTML = '';
+        articlesList.appendChild(noArticle);
+    } else {
+        articles.forEach(article => {
+            localDate = new Date(article['publish-date']).toLocaleDateString()
+
+            const newArticle = document.createElement('li');
+
+            const newId = document.createElement('p');
+            newId.textContent = `ID: ${article['article-id']}`;
+            newId.setAttribute('class', 'article-id');
+            newArticle.appendChild(newId);
+
+            const newTitleLink = document.createElement('a');
+            const url = `/open/${article['article-id']}`;
+            newTitleLink.setAttribute('href', url);
+
+            const newTitle = document.createElement('h4');
+            newTitle.textContent = article['article-title'];
+            newTitleLink.appendChild(newTitle);
+
+            const newDate = document.createElement('p');
+            newDate.textContent = `Data de publicação: ${localDate}`;
+
+            newArticle.appendChild(newId);
+            newArticle.appendChild(newTitleLink);
+            newArticle.appendChild(newDate);
+
             articlesList.appendChild(newArticle);
         })
+    }
+}
+
+function searchArticles(event, articles, searchForm) {
+    event.preventDefault();
+
+    const searchContent = searchForm.querySelector('input').value;
+    const searchSelect = searchForm.querySelector('select').value;
+    let url;
+
+    if (!searchContent.length) {
+        loadArticles(articles);
+    } else {
+        if (searchSelect === 'title') {
+            url = `/articles/title/${searchContent}`
+        } else if (searchSelect === 'id') {
+            url = `/articles/id/${searchContent}`
+        }
+
+    fetch(url, {
+            method: 'GET',
     })
-    .catch(error => console.error('Erro: ', error));
+    .then(res => res.json())
+    .then(articlesRes => {
+        loadArticles(articlesRes);
+    })
+    .catch(error => console.error('Erro na filtragem dos artigos pelo ID: ' + error));
+    }
 }
 
 // Procedimento a partir do carregamento do DOM
-document.addEventListener('DOMContentLoaded', () => {
-    loadArticles();
+document.addEventListener('DOMContentLoaded', async () => {
+    let articles = await fetch('/articles', {
+        method: 'GET',
+    })
+    .catch(error => {
+        console.error('Erro no carregamento dos artigos: ' + error)
+    })
+
+    articles = await articles.json();
+    loadArticles(articles);
 
     const addButton = document.getElementById('add-button');
     const removeButton = document.getElementById('remove-button');
@@ -30,26 +87,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const removeOverlay = document.getElementById('remove-popup-overlay');
     const addForm = document.getElementById('add-form');
     const removeForm = document.getElementById('remove-form');
+    const searchForm = document.getElementById('search-form');
+
     const cancelButtons = document.getElementsByClassName('cancel-button');
+    const searchButton = document.getElementById('search-button');
     const popups = document.getElementsByClassName('popup');
 
     // Função para abertura do pop-up de adição de artigo
-    addButton.addEventListener('click', () => addOverlay.style.display = 'flex');
+    addButton.addEventListener('click', () => {
+        addOverlay.style.display = 'flex'
+    });
 
     // Função para abertura do pop-up de remoção de artigo
-    removeButton.addEventListener('click', () => removeOverlay.style.display = 'flex');
-    
+    removeButton.addEventListener('click', () => {
+        removeOverlay.style.display = 'flex';
+    })
+
     // Função para fechamento dos pop-ups
-    Array.from(cancelButtons).forEach(button => 
-        button.addEventListener('click', () => 
-            button.parentElement.parentElement.style.display = 'none'));
-    
+    Array.from(cancelButtons).forEach(button => {
+        button.addEventListener('click', () => {
+            button.parentElement.parentElement.style.display = 'none';
+        })
+    })
+
     Array.from(popups).forEach(popup => {
         document.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') popup.parentElement.style.display = 'none';
         })
     })
     
+    searchButton.addEventListener('click', () => {
+        if (searchButton.parentElement.style.marginLeft === '0rem') {
+            searchButton.parentElement.style.marginLeft = '31rem';
+        } else {
+            searchButton.parentElement.style.marginLeft = '0rem';
+        }
+    })
+
     // Função para adição de artigo no banco de dados
     addForm.addEventListener('submit', async (event) => {
         event.preventDefault();
@@ -72,16 +146,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
             return res.json();
         })
-        .then(data => {
+        .then(async data => {
             console.log('Sucesso: ', data);
             addForm.reset();
+            articles = await (await fetch('/articles')).json();
+            loadArticles(articles);
         })
         .catch(error => {
             console.error('Erro: ', error);
             alert('Erro ao enviar o formulário: ' + error.message);
         })
 
-        loadArticles();
         addOverlay.style.display = 'none';
     })
 
@@ -89,8 +164,8 @@ document.addEventListener('DOMContentLoaded', () => {
     removeForm.addEventListener('submit', async (event) => {
         event.preventDefault();
 
-        const articleId = document.getElementById('remove-input').value
-        const url = `/articles/${articleId}`
+        const articleId = document.getElementById('remove-input').value;
+        const url = `/articles/${articleId}`;
 
         await fetch(url, {
             method: 'DELETE',
@@ -100,9 +175,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             return res.json();
         })
-        .then(data => {
+        .then(async data => {
             console.log('Sucesso: ', data);
             removeForm.reset();
+            articles = await (await fetch('/articles')).json();
+            loadArticles(articles);
         })
         .catch(error => {
             console.error('Erro: ', error);
@@ -110,7 +187,11 @@ document.addEventListener('DOMContentLoaded', () => {
             removeOverlay.style.display = 'none';
         })
 
-        loadArticles();
         removeOverlay.style.display = 'none';
     })
+
+    searchForm.querySelector('input')
+    .addEventListener('input', (event) => searchArticles(event, articles, searchForm));
+    searchForm.querySelector('select')
+    .addEventListener('input', (event) => searchArticles(event, articles, searchForm));
 })
