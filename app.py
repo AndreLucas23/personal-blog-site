61eb051f48f030617442475fdb13e165b4061787
André Lucas <sl.andrelucas@gmail.com>
2026-05-19 18:31:16 -0300

    feat: add docker infrastructure

--- app.py
+++ app.py
@@ -2,61 +2,23 @@ from flask import Flask, render_template, url_for, jsonify
 import mysql.connector
 from flask_cors import CORS
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='1234',
-        database='BLOG'
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
     )
-
-def tuple_to_dict(old_tuple):
-    if not old_tuple:
-        return dict()
-
-    new_dict = dict()
-    for i, attr in enumerate(old_tuple):
-        match i:
-            case 0:
-                new_dict['article-id'] = attr
-            case 1:
-                new_dict['article-title'] = attr
-            case 2:
-                new_dict['article-content'] = attr
-            case 3:
-                new_dict['publish-date'] = attr
-
-    return new_dict
-
-def tuple_to_list(old_tuple):
-    articles_list = []
-
-    for article in old_tuple:
-        article_dict = {'article-id': '',
-                       'article-title': '',
-                       'article-content': '',
-                       'publish-date': ''
-                        }
-
-        for i, attr in enumerate(article):
-            match i:
-                case 0:
-                    article_dict['article-id'] = attr
-                case 1:
-                    article_dict['article-title'] = attr
-                case 2:
-                    article_dict['article-content'] = attr
-                case 3:
-                    article_dict['publish-date'] = attr
-
-        articles_list.append(article_dict)
-
-    return articles_list
 
 from home import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from app import app, get_db_connection, tuple_to_dict, tuple_to_list
+from app import app, get_db_connection
 from flask import render_template, jsonify, Response, request, redirect, url_for
 
 @app.route('/')
@@ -8,36 +8,13 @@ def home():
 @app.route('/articles', methods=['GET'])
 def get_articles():
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE \
     FROM ARTICLES')
     all_articles = cursor.fetchall()
     conn.close()
 
-    articles_list = []
-
-    for article in all_articles:
-        article_dict = {'article-id': '', 
-                       'article-title': '',
-                       'article-content': '',
-                       'publish-date': ''
-                        }
-
-        for i, attr in enumerate(article):
-            match i:
-                case 0:
-                    article_dict['article-id'] = attr
-                case 1:
-                    article_dict['article-title'] = attr
-                case 2:
-                    article_dict['article-content'] = attr
-                case 3:
-                    article_dict['publish-date'] = attr
-
-        articles_list.append(article_dict)
-
-
-    return jsonify(articles_list)
+    return jsonify(all_articles)
 
 @app.route('/articles', methods=['POST'])
 def add_article():
@@ -68,38 +45,32 @@ def remove_article(remove_id):
 @app.route('/articles/id/<int:search_id>', methods=['GET'])
 def search_by_id(search_id):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
     'FROM ARTICLES WHERE ARTICLE_ID LIKE %s', (f'{search_id}%',))
     search_articles = cursor.fetchall()
     conn.close()
 
-    articles_list = tuple_to_list(search_articles)
-
-    return jsonify(articles_list), 200
+    return jsonify(search_articles), 200
 
 @app.route('/articles/title/<string:search_title>', methods=['GET'])
 def search_by_title(search_title):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
     'FROM ARTICLES WHERE ARTICLE_TITLE LIKE %s', (f'%{search_title}%',))
     search_articles = cursor.fetchall()
     conn.close()
 
-    articles_list = tuple_to_list(search_articles)
-
-    return jsonify(articles_list), 200
+    return jsonify(search_articles), 200
 
 @app.route('/open/<int:article_id>')
 def open_article(article_id):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
     'FROM ARTICLES WHERE ARTICLE_ID = %s', (article_id,))
     article = cursor.fetchone()
     conn.close()
-
-    article = tuple_to_dict(article)
 
     return render_template('article.html', article=article), 200
