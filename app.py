6c47e911ccb57e096870d31e47e135e434bcc095
André Lucas <sl.andrelucas@gmail.com>
2026-05-19 22:48:19 -0300

    feat: refining error handling scheme

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import mysql.connector
+
+import logging
 
 from home import home_bp
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(home_bp)
+
+@app.errorhandler(mysql.connector.Error)
+def handle_db_error(error):
+    logging.error(f'Erro do banco de dados: [{error.errno}] {error.msg}')    
+
+    return jsonify({
+        'status': 'error',
+        'message': 'Houve um erro interno ao processar os dados do banco'
+    }), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- home.py
+++ home.py
@@ -15,12 +15,17 @@ def home():
 
 @home_bp.route('/articles', methods=['GET'])
 def get_articles():
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE \
-    FROM ARTICLES')
-    all_articles = cursor.fetchall()
-    conn.close()
+    all_articles = []
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE \
+        FROM ARTICLES')
+        all_articles = cursor.fetchall()
+    except:
+        conn.close()
+        all_articles = []
 
     return jsonify(all_articles)
 
@@ -30,55 +35,65 @@ def add_article():
     article_title = data.get('new-title')
     article_content = data.get('new-content')
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO ARTICLES(ARTICLE_TITLE, ARTICLE_CONTENT) ' \
-    'VALUES (%s, %s)', (article_title, article_content))
-    conn.commit()
-    conn.close()
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO ARTICLES(ARTICLE_TITLE, ARTICLE_CONTENT) ' \
+        'VALUES (%s, %s)', (article_title, article_content))
+        conn.commit()
+    except:
+        conn.close()
 
     return jsonify({'message': 'artigo criado com sucesso'}), 201
 
 @home_bp.route('/articles/<int:remove_id>', methods=['DELETE'])
 def remove_article(remove_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM ARTICLES ' \
-    'WHERE ARTICLE_ID = %s', (remove_id,))
-    conn.commit()
-    conn.close()
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM ARTICLES ' \
+        'WHERE ARTICLE_ID = %s', (remove_id,))
+        conn.commit()
+    finally:
+        conn.close()
 
     return jsonify({'message': 'artigo removido com sucesso'}), 200
 
 @home_bp.route('/articles/id/<int:search_id>', methods=['GET'])
 def search_by_id(search_id):
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
-    'FROM ARTICLES WHERE ARTICLE_ID LIKE %s', (f'{search_id}%',))
-    search_articles = cursor.fetchall()
-    conn.close()
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
+        'FROM ARTICLES WHERE ARTICLE_ID LIKE %s', (f'{search_id}%',))
+        search_articles = cursor.fetchall()
+    finally:
+        conn.close()
 
     return jsonify(search_articles), 200
 
 @home_bp.route('/articles/title/<string:search_title>', methods=['GET'])
 def search_by_title(search_title):
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
-    'FROM ARTICLES WHERE ARTICLE_TITLE LIKE %s', (f'%{search_title}%',))
-    search_articles = cursor.fetchall()
-    conn.close()
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
+        'FROM ARTICLES WHERE ARTICLE_TITLE LIKE %s', (f'%{search_title}%',))
+        search_articles = cursor.fetchall()
+    finally:
+        conn.close()
 
     return jsonify(search_articles), 200
 
 @home_bp.route('/open/<int:article_id>')
 def open_article(article_id):
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
-    'FROM ARTICLES WHERE ARTICLE_ID = %s', (article_id,))
-    article = cursor.fetchone()
-    conn.close()
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT ARTICLE_ID, ARTICLE_TITLE, ARTICLE_CONTENT, PUBLISH_DATE ' \
+        'FROM ARTICLES WHERE ARTICLE_ID = %s', (article_id,))
+        article = cursor.fetchone()
+    finally:
+        conn.close()
 
     return render_template('article.html', article=article), 200
