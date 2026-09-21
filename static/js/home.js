a82a6b27e8f3ef657e508119c0027e06bd3af8a2
André Lucas <sl.andrelucas@gmail.com>
2026-09-21 11:23:19 -0300

    style: standardize attribute ordering and indentation

--- static/js/home.js
+++ static/js/home.js
@@ -61,7 +61,10 @@ const articlesUI = {
         const li = document.createElement('li');
         li.classList.add('articles__empty');
         li.innerHTML = `
-            <svg class="articles__empty-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" aria-hidden="true">
+            <svg class="articles__empty-icon"
+            aria-hidden="true"
+            viewBox="0 0 640 640"
+            xmlns="http://www.w3.org/2000/svg">
                 <path d="M32 176C32 134.5 63.6 100.4 104 96.4L104 96L384 96C437 96 480 139 480 192L480 368L304 368C264.2 368 232 400.2 232 440L232 500C232 524.3 212.3 544 188 544C163.7 544 144 524.3 144 500L144 272L80 272C53.5 272 32 250.5 32 224L32 176zM268.8 544C275.9 530.9 280 515.9 280 500L280 440C280 426.7 290.7 416 304 416L552 416C565.3 416 576 426.7 576 440L576 464C576 508.2 540.2 544 496 544L268.8 544zM112 144C94.3 144 80 158.3 80 176L80 224L144 224L144 176C144 158.3 129.7 144 112 144z"/>
             </svg>
             <span>Nenhum artigo encontrado.</span>
@@ -74,7 +77,10 @@ const articlesUI = {
         const li = document.createElement('li');
         li.classList.add('articles__empty');
         li.innerHTML = `
-            <svg class="articles__empty-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" aria-hidden="true">
+            <svg class="articles__empty-icon"
+            aria-hidden="true"
+            viewBox="0 0 640 640"
+            xmlns="http://www.w3.org/2000/svg">
                 <path d="M32 176C32 134.5 63.6 100.4 104 96.4L104 96L384 96C437 96 480 139 480 192L480 368L304 368C264.2 368 232 400.2 232 440L232 500C232 524.3 212.3 544 188 544C163.7 544 144 524.3 144 500L144 272L80 272C53.5 272 32 250.5 32 224L32 176zM268.8 544C275.9 530.9 280 515.9 280 500L280 440C280 426.7 290.7 416 304 416L552 416C565.3 416 576 426.7 576 440L576 464C576 508.2 540.2 544 496 544L268.8 544zM112 144C94.3 144 80 158.3 80 176L80 224L144 224L144 176C144 158.3 129.7 144 112 144z"/>
             </svg>
             <span>Não foi possível carregar os artigos.<br>Verifique sua conexão e recarregue a página.</span>
